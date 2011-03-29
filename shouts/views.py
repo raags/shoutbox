1aba277707a15e42122d341302e4867f51f3ec0b
Raghu Udiyar <raghusiddarth@gmail.com>
2011-03-30 00:25:04 +0530

    Prepare shout_list view for object creation

--- shouts/views.py
+++ shouts/views.py
@@ -1,12 +1,23 @@
 from django.views.generic.list_detail import object_list
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from models import Shout
 
 def shout_list(request, notice = ""):
-	"""Show all shouts"""
-	return object_list(request, 
-	queryset = Shout.objects.all(),
-	template_object_name = 'shouts', 
-	extra_context = {'notice': notice}
+	
+	if 'shout' in request.POST and request.POST['shout']:
+    		return render_to_response('shouts/shout_list.html', {
+		'notice': "add content",
+		}, context_instance=RequestContext(request))
+
+	else:			
+		"""Show all shouts"""
+
+		return object_list(request, 
+		queryset = Shout.objects.all(),
+		template_object_name = 'shouts', 
+		extra_context = {'notice': notice}
 )
 
 def shout_post(request):
