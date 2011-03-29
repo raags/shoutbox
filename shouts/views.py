5c6e5bd54d9b8bd51a16b68e64fd4b8b6c13b9c4
Raghu Udiyar <raghusiddarth@gmail.com>
2011-03-30 00:46:11 +0530

    Basic functionality completed

--- shouts/views.py
+++ shouts/views.py
@@ -1,5 +1,6 @@
+from django.core.urlresolvers import reverse
 from django.views.generic.list_detail import object_list
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from models import Shout
@@ -7,13 +8,16 @@ from models import Shout
 def shout_list(request, notice = ""):
 	
 	if 'shout' in request.POST and request.POST['shout']:
-    		return render_to_response('shouts/shout_list.html', {
-		'notice': "add content",
-		}, context_instance=RequestContext(request))
+		s = Shout(post_as = request.POST['poster'], content = request.POST['shout'])
+    		s.save()
+		return  HttpResponseRedirect(reverse('shouts.views.shout_list'))
+	
+#		return render_to_response('shouts/shout_list.html', {
+#		'notice': "add content",
+#		}, context_instance=RequestContext(request))
 
 	else:			
 		"""Show all shouts"""
-
 		return object_list(request, 
 		queryset = Shout.objects.all(),
 		template_object_name = 'shouts', 
